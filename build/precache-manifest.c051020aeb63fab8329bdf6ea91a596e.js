a0c7d59400868d9c034842664ec5ce7a212b4aaf
alain <alaing.dev@gmail.com>
2019-09-10 22:21:26 -0400

    change photo library path

---
+++ build/precache-manifest.c051020aeb63fab8329bdf6ea91a596e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ada957d54815e2dd8b08d5de3f887f",
+    "url": "/alain_green_photography/index.html"
+  },
+  {
+    "revision": "1c7ae323f9a3ece9f5e1",
+    "url": "/alain_green_photography/static/css/2.5decf86f.chunk.css"
+  },
+  {
+    "revision": "96c3f17a71cb6c5ff908",
+    "url": "/alain_green_photography/static/css/main.6837cb9d.chunk.css"
+  },
+  {
+    "revision": "1c7ae323f9a3ece9f5e1",
+    "url": "/alain_green_photography/static/js/2.f64756dd.chunk.js"
+  },
+  {
+    "revision": "96c3f17a71cb6c5ff908",
+    "url": "/alain_green_photography/static/js/main.75abb2de.chunk.js"
+  },
+  {
+    "revision": "2223bb68597378102f9a",
+    "url": "/alain_green_photography/static/js/runtime~main.6cd60084.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/alain_green_photography/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/alain_green_photography/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/alain_green_photography/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/alain_green_photography/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/alain_green_photography/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/alain_green_photography/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/alain_green_photography/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/alain_green_photography/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/alain_green_photography/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/alain_green_photography/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/alain_green_photography/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/alain_green_photography/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/alain_green_photography/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/alain_green_photography/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/alain_green_photography/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/alain_green_photography/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
